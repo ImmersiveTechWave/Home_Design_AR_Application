ce5163bfc9eaa82bcc71db416ca805ae4affb4e7
Alexandru <alexandru.franciuc@tractormedia.com>
2022-04-01 22:56:38 +0300

    feat: add base state manager

---
+++ Licenta-A/Assets/Scripts/Runtime/Core/GameStateManager.cs
@@ -0,0 +1,119 @@
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using System;
+
+namespace AF
+{
+	public class GameStateManager : MonoBehaviour
+	{
+		public readonly Type DEFAULT_GAME_STATE = typeof(CreateWallState);
+
+		public static GameStateManager Instance { get { return instance; } }
+
+		public Type CurrentState { get; private set; }
+		public event Action<BaseGameState> GameStateChanged;
+
+		private static GameStateManager instance;
+		private Stack<BaseGameState> gameStateStack = new Stack<BaseGameState>();
+		private Dictionary<Type, BaseGameState> stateDictionary = new Dictionary<Type, BaseGameState>();
+
+		public void Awake()
+		{
+			if (instance == null)
+			{
+				instance = this;
+				DontDestroyOnLoad(gameObject);
+			}
+			else
+			{
+				if (instance != this)
+				{
+					Destroy(gameObject);
+				}
+			}
+
+			InitGameStates();
+		}
+
+		private void InitGameStates()
+		{
+			var baseStateType = typeof(BaseGameState);
+			Func<Type, bool> baseStatePredicate;
+
+			baseStatePredicate = t => { return !t.IsInterface && !t.IsAbstract && baseStateType.IsAssignableFrom(t); };
+
+			stateDictionary.Clear();
+			var gameStateTypes = TypesUtils.GetTypes(baseStatePredicate);
+			foreach (var type in gameStateTypes)
+			{
+				try
+				{
+					var addedState = new GameObject(type.Name).AddComponent(type);
+					addedState.transform.SetParent(transform);
+
+					var gameState = addedState as BaseGameState;
+					stateDictionary.Add(type, gameState);
+				}
+				catch (Exception e)
+				{
+					throw e;
+				}
+			}
+
+			// Init with the default state.
+			PushState(gameStateTypes.FirstOrDefault(p => p == DEFAULT_GAME_STATE));
+		}
+
+		public void SwitchState<T>() where T : BaseGameState
+		{
+			SwitchState(typeof(T));
+		}
+
+		public void SwitchState(Type type)
+		{
+			var gameState = FindState(type);
+
+			if (gameState == null)
+			{
+				return;
+			}
+
+			var previousState = gameStateStack.Count > 0 ? gameStateStack.Pop() : null;
+			gameStateStack.Push(gameState);
+
+			previousState?.Exit(gameState);
+			gameState.Enter(previousState);
+			CurrentState = type;
+			GameStateChanged?.Invoke(gameState);
+		}
+
+		private BaseGameState FindState(Type type)
+		{
+			if (!stateDictionary.TryGetValue(type, out var state))
+			{
+				return null;
+			}
+			return state;
+		}
+
+		private void PushState(Type type)
+		{
+			if (!stateDictionary.TryGetValue(type, out var state))
+			{
+				return;
+			}
+
+			if (gameStateStack.Count > 0)
+			{
+				gameStateStack.Peek().Exit(state);
+				state.Enter(gameStateStack.Peek());
+			}
+			else
+			{
+				state.Enter(null);
+			}
+			gameStateStack.Push(state);
+		}
+	}
+}
---
+++ Licenta-A/Assets/Scripts/Runtime/States/CreateWallState.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AF
+{
+	public class CreateWallState : BaseGameState
+	{
+
+	}
+}
